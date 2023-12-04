1099ab6a44c3878ecdc54797053371297a2db8c0
Prophet Bestman <bestiproph@gmail.com>
2023-12-04 09:00:06 +0100

    fix: fixed imports in main index

--- src/components/Main/index.ts
+++ src/components/Main/index.ts
@@ -1,4 +1,3 @@
-export { default as MainHeader } from "./MainHeader";
 export { default as MainStatsCard } from "./MainStatsCard";
 export { default as Recommendation } from "./Recommendation";
 export { default as ResponseActivity } from "./ResponseActivity";
