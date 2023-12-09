f35ef4b6df6bf22758e7f44cf38fd3f100e7f5f8
Prophet Bestman <bestiproph@gmail.com>
2023-12-09 20:47:13 +0100

    feat: landing page banner section

--- src/components/Landing/BannerSection.tsx
+++ src/components/Landing/BannerSection.tsx
@@ -1,3 +1,29 @@
+import { Button } from "../ui/button";
+
 export default function BannerSection() {
-  return <div>BannerSection</div>;
+  return (
+    <div className="px-[100px] grid grid-cols-2">
+      <div className="flex flex-col justify-center space-y-8">
+        <h1 className="leading-tight text-white font-medium text-[60px]">
+          Empowering Citizens through Digital Engagement
+        </h1>
+
+        <p className="text-2xl text-subtle_text">
+          Gift yourself, your family, a friend or a boo a Bion card to manage
+          their spendings
+        </p>
+
+        <div className="flex gap-8">
+          <Button className="w-[175px]">Get Started</Button>
+          <Button className="w-[175px]" variant="outline-primary">
+            Learn more
+          </Button>
+        </div>
+      </div>
+
+      <div className="flex justify-end">
+        <img src="/public/images/landing/landing-banner.svg" alt="" />
+      </div>
+    </div>
+  );
 }
