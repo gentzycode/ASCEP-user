30a9b53b34ecbb0d6a1bd412946b3b4b353f3cbd
gentzycode <gentzycode@live.com>
2023-12-12 01:37:42 +0100

    Few updates for presentation

--- src/components/Landing/AboutUsSection.tsx
+++ src/components/Landing/AboutUsSection.tsx
@@ -1,32 +1,28 @@
 export default function AboutUsSection() {
   return (
     <div id="about-us" className="px-[100px] grid grid-cols-11">
-      <div className="flex justify-center col-span-5">
-        <img src="/images/landing/large-logo.svg" alt="" />
+      <div className="flex justify-center col-span-5 relative">
+        <img src="/images/landing/anambra_citizens.png" alt="" className="anambra-citizens-image" />
+        <div className="overlay"></div>
       </div>
 
       <div className="col-span-6">
         <div className="w-full max-w-[633px]">
-          <p className="text-lg uppercase text-primary">About us</p>
+          <p className="text-lg uppercase text-primary">ASCEP</p>
 
           <p className="mt-3 font-bold mb-6 leading-tight text-[36px] text-white">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            Anambra State Citizens Engagement Portal
           </p>
 
           <div className="text-xl text-subtitle_text">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
-            <p>
-              Duis aute irure dolor in reprehenderit in voluptate velit esse
-              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-              cupidatat non proident, sunt in culpa qui officia deserunt mollit
-              anim id est laborum.
-            </p>
+              <p>
+                Governor Soludo has initiated a pioneering step to revolutionize governance by actively engaging Anambra's citizens. Recognizing the transformative power of digital platforms, he has launched this portal to bridge the gap between the government and its people, ensuring every voice can be heard.
+              </p>
+              <p>
+                This solution is more than a platform; it's a commitment to participatory democracy, enabling transparent dialogue, fostering collaborative decision-making, and allowing citizens to contribute constructively to state development.
+              </p>
           </div>
+
         </div>
       </div>
     </div>
