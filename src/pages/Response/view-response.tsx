7b88fc8b6f77a4b12fee6a8d7ebba334c406070d
Prophet Bestman <bestiproph@gmail.com>
2024-01-08 20:14:46 +0100

    feat: fetch report details

--- src/pages/Response/view-response.tsx
+++ src/pages/Response/view-response.tsx
@@ -1,18 +1,31 @@
+import { useGetReportInfo } from "@/api/response";
 import {
   ResponseComment,
   ResponseDetails,
   ResponseImageSelect,
 } from "@/components/Response";
+import { FloatingLoader } from "@/components/custom";
+import { useParams } from "react-router-dom";
 
 export default function ViewResponsepage() {
+  const { reportId } = useParams();
+
+  const { data, isLoading } = useGetReportInfo(reportId!);
   return (
     <div className="min-h-screen px-6 space-y-7">
-      <h3 className="text-2xl">View Response</h3>
-      <ResponseImageSelect />
-      <ResponseDetails />
-      <ResponseComment />
-      <ResponseComment />
-      <ResponseComment />
+      {isLoading && <FloatingLoader />}
+      {data && (
+        <>
+          <h3 className="text-2xl">View Response</h3>
+          {!!data.reportImages.length && (
+            <ResponseImageSelect images={data?.reportImages} />
+          )}
+          <ResponseDetails report={data} />
+          <ResponseComment />
+          <ResponseComment />
+          <ResponseComment />
+        </>
+      )}
     </div>
   );
 }
