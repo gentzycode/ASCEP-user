06eb4253c5bd8f1adfd0f0ca7635028c9b6d2eac
Prophet Bestman <bestiproph@gmail.com>
2024-01-19 05:42:20 +0100

    chore: remove search and notifications icons

--- src/components/layout-components/UserDropdown.tsx
+++ src/components/layout-components/UserDropdown.tsx
@@ -6,10 +6,10 @@ import {
 import {
   ArrowDown2,
   ArrowRight2,
-  Notification,
-  SearchNormal1,
+  // Notification,
+  // SearchNormal1,
 } from "iconsax-react";
-import { IconWrapper } from "../custom";
+// import { IconWrapper } from "../custom";
 import { useAppContext } from "@/contexts/AppContext";
 import useLogout from "@/hooks/useLogout";
 
@@ -21,12 +21,12 @@ export default function UserDropdown() {
     <Popover>
       <PopoverTrigger asChild>
         <div className="flex items-center gap-6">
-          <IconWrapper className="rounded-full cursor-pointer">
+          {/* <IconWrapper className="rounded-full cursor-pointer">
             <SearchNormal1 size="20" color="black" />
           </IconWrapper>
           <IconWrapper className="rounded-full cursor-pointer">
             <Notification size="20" color="black" />
-          </IconWrapper>
+          </IconWrapper> */}
 
           {user && (
             <div className="flex items-center gap-3 cursor-pointer">
